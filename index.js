6fd558b666a714c8b6c43f4d19d443f008867168
ykurata <yasukokurata@gmail.com>
2020-03-17 16:00:11 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 5000;
+const db = require('./queries');
+
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+app.get('/', (req, res) => {
+  res.json({ info: 'Node, Express, Postgres api'});
+})
+
+// Set up routes
+app.get('/users', db.getUsers)
+app.get('/users/:id', db.getUserById)
+app.post('/users', db.createUser)
+app.put('/users/:id', db.updateUser)
+app.delete('/users/:id', db.deleteUser)
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+})
